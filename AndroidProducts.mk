8e814823663414e387574a09036da60d2e0e64ed
Deivid21 <david.parra.ignacio@gmail.com>
2020-11-12 01:27:38 -0300

    rr-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_river.mk
+    $(LOCAL_DIR)/rr_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_river-eng \
-    lineage_river-user \
-    lineage_river-userdebug
+    rr_river-eng \
+    rr_river-user \
+    rr_river-userdebug
