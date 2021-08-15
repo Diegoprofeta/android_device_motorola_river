9f6f64f5f2dff72be86ff1fd13d9714f709e9bee
Diegoprofeta <diegosilvasantos1983@gmail.com>
2021-08-15 10:19:38 -0300

    river: bringup to stellar-S1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_river.mk
+    $(LOCAL_DIR)/stellar_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_river-eng \
-    lineage_river-user \
-    lineage_river-userdebug
+    stellar_river-eng \
+    stellar_river-user \
+    stellar_river-userdebug
